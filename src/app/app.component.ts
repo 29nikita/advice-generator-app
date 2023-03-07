26e6b218040853b31dbc28619a90b266261fbcb1
Nikita Agarwal <nikitaagr313@gmail.com>
2023-03-07 16:57:04 +0530

    intial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AppService } from "./app.service";
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,19 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'advice-generator-app';
+  response: any;
+
+  constructor(private appService: AppService) { }
+  ngOnInit() {
+
+    this.onClickRandomAdviceGenerator();
+
+  }
+
+  onClickRandomAdviceGenerator() {
+    this.appService.getRandomAdvice().subscribe((data) => {
+      this.response = data;
+    })
+  }
+
 }
